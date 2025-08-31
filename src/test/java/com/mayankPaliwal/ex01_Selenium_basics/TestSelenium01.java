e003b92b8bb4ba1947491b45c54ed476e06a4cef
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-08-31 23:36:27 +0530

    web driver first test

---
+++ src/test/java/com/mayankPaliwal/ex01_Selenium_basics/TestSelenium01.java
@@ -0,0 +1,26 @@
+package com.mayankPaliwal.ex01_Selenium_basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class TestSelenium01 {
+
+    @Test
+    public void test_code(){
+        Assert.assertEquals("mayank","mayank");
+    }
+
+
+    @Test
+    public void test_OpenTheTestingAcademy(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        driver.close();
+
+    }
+}
